f882621ef102fd451088b4f7038252781538eda9
anujsoni08 <anujsoni08@gmail.com>
2020-12-19 13:38:42 +0530

    added basic webpack setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+// import './style.css'
+import "./style.scss";
+
+const App = () => {
+  return <h1>Hello world</h1>;
+};
+
+reactDOM.render(<App />, document.getElementById("root"));
